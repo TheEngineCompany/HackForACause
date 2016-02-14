4647bc3faf002dbbc4690e4c590431fde131d70a
Larry Price <laprice@gmail.com>
2016-02-13 19:10:52 -0800

    adding a root index.

--- wayfinding/wayfinding/urls.py
+++ wayfinding/wayfinding/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     url(r'^dwtn/', include('dwtn.urls')),
     url(r'^admin/', admin.site.urls),
 ]
