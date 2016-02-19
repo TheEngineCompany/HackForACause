6c0b66d18dbdfbc88783fc26957a2b30a7eb1d97
gandalf3 <zzyxpaw@gmail.com>
2016-02-19 02:23:37 -0800

    giant OOPification refactorization \o/

--- wayfinding/static/js/downtown.js
+++ wayfinding/static/js/downtown.js
@@ -47,89 +47,112 @@ function activate_marker(e) {
 	}
 }
 
-function category(name, color) {
+function Category(name, color, id) {
 	this.name = name;
 	this.color = color;
+	this.id = id;
+	this.icon = null;
+	this.attractions = [];
+	
+	var catclass = "category-" + this.id;
+	var firstletter = this.name.charAt(0).toUpperCase();
+	this.icon = L.divIcon({
+		"className": "marker " + catclass,
+		"html": firstletter
+	});
 	
 	var li = document.createElement("li");
 	li.classList.add("category");
-	li.setAttribute("style", "background-color:" + category.color + ";");
-	li.setAttribute("data-category-id", category.id);
+	li.setAttribute("style", "background-color:" + this.color + ";");
+	li.setAttribute("data-category-id", this.id);
 	
 	var title = document.createElement("h3");
-	title.textContent = category.name;
+	title.textContent = this.name;
 	li.appendChild(title);
 	
 	var locations = document.createElement("ul");
 	locations.classList.add("locations");
-	locations.setAttribute("id", 'category_' + category.id + '_loclist');
+	locations.setAttribute("id", 'category_' + this.id + '_loclist');
 	li.appendChild(locations);
 	
-	document.getElementById('categories').appendChild(cat_li);
+	document.getElementById('categories').appendChild(li);
 	
 }
 
-function attraction(lat, lon, category, name, details, image) {
-	this.coordinates = L.LatLng(lat, lon);
-	this.category = data.categories.filter(function(c) { return c.id == data.locations[i].catid })[0];
+function Attraction(lat, lon, category, name) {
+	this.coordinates = new L.LatLng(lat, lon);
+	this.category = category;
 	this.name = name;
-	this.details = details;
-	this.imagePath = image;
+	this.details = null;
+	this.imageUrl = null;
 
 	var li = document.createElement("li");
 	li.classList.add("location");
-	li.textContent = data.locations[i].name;
+	li.textContent = this.name;
 	document.getElementById('category_' + category.id + '_loclist').appendChild(li);
-
-	this.marker = L.marker(coords, { "title": data.locations[i].name, "icon": category.icon, "riseOnHover": true })
-	this.marker.on("click", activate_marker)
+	
+	this.marker = L.marker(this.coordinates, { "title": this.name, "icon": this.category.icon, "riseOnHover": true })
+	console.log(this.marker)
 	this.marker.addTo(map)
 }
 
-function render_category(category) {
-	
-	var cat_li = document.createElement("li");
-	cat_li.classList.add("category");
-	cat_li.setAttribute("style", "background-color:" + category.color + ";");
-	cat_li.setAttribute("data-id", category.id);
-	
-	var cat_title = document.createElement("h3");
-	cat_title.textContent = category.name;
-	cat_li.appendChild(cat_title);
-	
-	var loc_list = document.createElement("ul");
-	loc_list.classList.add("locations");
-	loc_list.setAttribute("id", 'category_' + category.id + '_loclist');
-	cat_li.appendChild(loc_list);
-	
-	document.getElementById('categories').appendChild(cat_li);
-}
+// function render_category(category) {
+//
+// 	var cat_li = document.createElement("li");
+// 	cat_li.classList.add("category");
+// 	cat_li.setAttribute("style", "background-color:" + category.color + ";");
+// 	cat_li.setAttribute("data-id", category.id);
+//
+// 	var cat_title = document.createElement("h3");
+// 	cat_title.textContent = category.name;
+// 	cat_li.appendChild(cat_title);
+//
+// 	var loc_list = document.createElement("ul");
+// 	loc_list.classList.add("locations");
+// 	loc_list.setAttribute("id", 'category_' + category.id + '_loclist');
+// 	cat_li.appendChild(loc_list);
+//
+// 	document.getElementById('categories').appendChild(cat_li);
+// }
 
 function generate_menu(data) {
-	var categorized = []
+	// var categorized = []
 	console.log(data)
 	
 	for (var i=0; i<data.categories.length; i++) {
-		render_category(data.categories[i]);
-		var catclass = "marker_cat_" + data.categories[i].id
-		data.categories[i].icon = L.divIcon({"className": "marker " + catclass, "html": data.categories[i].name.charAt(0).toUpperCase() })
+		
+		var c = data.categories[i];
+		var category = new Category(c.name, c.color, c.id);
+
+		// get all attractions with an id coorisponding to the current category
+		var attractions = data.locations.filter(function(a) { return a.catid == c.id });
+		
+		for (var n=0; n<attractions.length; n++) {
+			// put attraction info into an Attraction object and put it in our category
+			var a = attractions[n];
+			var attraction = new Attraction(a.lat, a.lon, category, a.name);
+			attraction.details = a.details;
+			attraction.imageUrl = a.image;
+			
+			category.attractions.push(attraction);
+		}
 	}
-	for (var i=0; i<data.locations.length; i++) {
+	// for (var i=0; i<data.locations.length; i++) {
 		
-		var category = data.categories.filter(function(cat) {
-			return cat.id == data.locations[i].catid;
-		})[0];
-		
-		var loc_li = document.createElement("li");
-		loc_li.classList.add("location");
-		loc_li.textContent = data.locations[i].name;
-		document.getElementById('category_' + category.id + '_loclist').appendChild(loc_li);
-		
-		var coords = L.latLng(data.locations[i].lat, data.locations[i].lon);
-		var marker = L.marker(coords, { "title": data.locations[i].name, "icon": category.icon, "riseOnHover": true })
-		marker.on("click", activate_marker)
-		marker.addTo(map)
-	}
+		// var category = data.categories.filter(function(cat) {
+		// 	return cat.id == data.locations[i].catid;
+		// })[0];
+		//
+		// var loc_li = document.createElement("li");
+		// loc_li.classList.add("location");
+		// loc_li.textContent = data.locations[i].name;
+		// document.getElementById('category_' + category.id + '_loclist').appendChild(loc_li);
+		//
+		// var coords = L.latLng(data.locations[i].lat, data.locations[i].lon);
+		// var marker = L.marker(coords, { "title": data.locations[i].name, "icon": category.icon, "riseOnHover": true })
+		// marker.on("click", activate_marker)
+		// marker.addTo(map)
+	// }
 }
 
 function main(data) {
@@ -138,7 +161,7 @@ function main(data) {
 	
 	// set colors of markers
 	for (var i=0; i<data.categories.length; i++) {
-		var markers = document.getElementsByClassName('marker_cat_' + data.categories[i].id)
+		var markers = document.querySelectorAll('.marker.category-' + data.categories[i].id)
 		
 		for (var n = 0; n < markers.length; n++) {
 			markers[n].style.backgroundColor = data.categories[i].color;
